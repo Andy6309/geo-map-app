b29170dcf955b1a8668b87762d3107eccbe11029
Andy6309 <Andy6309@users.noreply.github.com>
2025-04-08 21:00:48 -0400

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import dynamic from "next/dynamic";
 
 // Dynamically import the JavaScript file
-const Map = dynamic(() => import("@/components/map.js"), { ssr: false });
+const Map = dynamic(() => import("@/components/map"), { ssr: false });
 
 export default function Home() {
     return (
